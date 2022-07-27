c8c5535af0a2b3dfdbb32cea86babf714b4ae16d
김도언 <vov06144@naver.com>
2022-07-28 00:34:59 +0900

    Upload study contents (0728)

--- test2_mythoughts.py
+++ test2_mythoughts.py
@@ -1,19 +1,5 @@
-def func(*args):
-    print(args)
-    print(type(args))
-
-func(1, 2, 3, 'a', 'b')
-
-def print_family_name(father, mother, *pets):
-    print(f'아버지 : {father}')
-    print(f'어머니 : {mother}')
-    print('반려동물들..')
-    for name in pets:
-        print(f'반려동물: {name}')
-
-print_family_name('아부지', '어무니')
-
-a = '135'
-while a:
-    print(a[-1])
-    a = a[:-1]
+print('''|\\_/|''')
+print('''|q p|   /}''')
+print('''( 0 )"""\\''')
+print('''|"^"`    |''')
+print('''||_/=\\\\__|''')
